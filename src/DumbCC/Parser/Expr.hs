1eb710a4c4d87408c6096ec8677db18de5416401
Astrid Yu <astrid@astrid.tech>
2022-10-19 00:13:24 -0700

    try to do the parser... still having trouble..

--- src/DumbCC/Parser/Expr.hs
+++ src/DumbCC/Parser/Expr.hs
@@ -6,53 +6,73 @@ module DumbCC.Parser.Expr where
 import Control.Monad.Except
 import Control.Monad.State
 import DumbCC.Lexer
-import qualified DumbCC.Lexer as L
 import DumbCC.Parser.Types.Sugared
-import qualified DumbCC.Parser.Types.Sugared as P
 
 parseExpr :: [Token] -> (Maybe ExprS)
 parseExpr = undefined
 
-data ParserState = ParserState
-  { exprs :: [(Bool, ExprS)],
-    ops :: [BOp],
+data PS = PS
+  { exprStack :: [(Bool, ExprS)],
+    opStack :: [BOp],
     wasBinary :: Bool
   }
 
+data StackState = 
+  StEx Bool ExprS
+  | StUn UOp
+  | StBi BOp
+  | StOpenParen
+
 newtype Parser a = Parser
-  { runParser :: ExceptT String (State ParserState) a
+  { runParser :: ExceptT String (State PS) a
   }
   deriving (Functor, Applicative, Monad)
 
-liftGet :: (ParserState -> a) -> Parser a
+instance MonadFail Parser where
+  fail err = Parser $ throwError err
+
+liftGet :: (PS -> a) -> Parser a
 liftGet f = Parser $ lift (f <$> get)
 
-state' :: (ParserState -> (a, ParserState)) -> Parser a
+state' :: (PS -> (a, PS)) -> Parser a
 state' f = Parser $ lift (state f)
 
-getExprs :: Parser [(Bool, ExprS)]
-getExprs = liftGet exprs
+getExprStack :: Parser [(Bool, ExprS)]
+getExprStack = liftGet exprStack
 
-getOps :: Parser [BOp]
-getOps = liftGet ops
+getOpStack :: Parser [BOp]
+getOpStack = liftGet opStack
 
 getWasBinary :: Parser Bool
 getWasBinary = liftGet wasBinary
 
-shift :: Token -> Parser (Either String ())
+setState :: PS -> Parser ()
+setState s = state' (\_ -> ((), s))
+
+shift :: Token -> Parser ()
 shift t = do
-  ge <- getExprs
-  o <- getOps
-  pure $ case (ge, o, t) of
-    ([], TId n) -> Right ([Ex (EId n)], ts)
-    ([], TNum l) -> Right ([Ex (ELit (LNum l))], ts)
-    ([], TStr s) -> Right ([Ex (ELit (LStr s))], ts)
-    ([], TPnc L.Add) -> Right ([SUn P.Add], ts)
-    ([], TPnc L.Sub) -> Right ([SUn P.Sub], ts)
-    ([], TPnc L.Mul) -> Right ([SUn P.Deref], ts)
-    ([], TPnc L.BAnd) -> Right ([SUn P.Ref], ts)
-    (ss', TPnc L.LParen) -> Right (OpenParen : ss', ts)
-    _ -> Left "Invalid start symbol"
+  es <- getExprStack
+  os <- getOpStack
+  case t of
+    TId n -> setState (PS ((False, EId n) : es) os False)
+    TStr s -> setState (PS ((False, ELit (LStr s)) : es) os False)
+    TNum x -> setState (PS ((False, ELit (LNum x)) : es) os False)
+    TPunct p -> case es of
+      [] -> let unary = case p of
+                                Add -> 
+
+      
+    _ -> fail "Parse failure"
+
+--    ([], TId n) -> Right ([Ex (EId n)], ts)
+--    ([], TNum l) -> Right ([Ex (ELit (LNum l))], ts)
+--    ([], TStr s) -> Right ([Ex (ELit (LStr s))], ts)
+--    ([], TPnc L.Add) -> Right ([SUn P.Add], ts)
+--    ([], TPnc L.Sub) -> Right ([SUn P.Sub], ts)
+--    ([], TPnc L.Mul) -> Right ([SUn P.Deref], ts)
+--    ([], TPnc L.BAnd) -> Right ([SUn P.Ref], ts)
+--    (ss', TPnc L.LParen) -> Right (OpenParen : ss', ts)
+--    _ -> Left "Invalid start symbol"
 
 -- reduce ss = case ss of
 --   (Ex _ x : SUn op : ss') -> (Ex False (EUn op x)) : ss'
