71428cd77883a299265c98c06774107afb711dda
Astrid Yu <astrid@astrid.tech>
2022-10-18 17:07:30 -0700

    Switch lexer to use regex

--- src/DumbCC/Lexer.hs
+++ src/DumbCC/Lexer.hs
@@ -1,15 +1,51 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module DumbCC.Lexer where
 
 import Control.Monad.State
-import Data.Char
+import Text.RawString.QQ
+import Text.Regex.TDFA
 
 data Token
-  = TKeyword String
-  | TId String
-  | TConst String
+  = TId String
+  | TFloat Double
+  | TInt Integer
   | TStr String
-  | TOp String
-  | TPunct String
+  | TOp Op
+  | TPunct Punct
+
+data Punct
+  = Comma
+  | Semicolon
+  | Colon
+  | LCurl
+  | RCurl
+  | LBrace
+  | RBrace
+  | LParen
+  | RParen
+
+data Op
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | BAnd
+  | BOr
+  | BNot
+  | LAnd
+  | LOr
+  | LNot
+  | Xor
+  | Inc
+  | Dec
+  | Asgn
+  | Eq
+  | Neq
+  | Ge
+  | Le
+  | Geq
+  | Leq
 
 lex :: String -> [Token]
 lex "" = []
@@ -26,8 +62,8 @@ peekChar = do
 peekPos :: Tokenizer Int
 peekPos = gets (\(i, _) -> i)
 
-readChar :: Tokenizer (Maybe Char)
-readChar = do
+takeChar :: Tokenizer (Maybe Char)
+takeChar = do
   (i, s) <- get
   case s of
     "" -> pure Nothing
@@ -35,26 +71,101 @@ readChar = do
       put (i + 1, xs)
       pure $ Just x
 
-readId :: Tokenizer String
-readId = do
-  res <- readChar
-  case res of
-    Just c ->
-      if isLetter c
-        then do
-          rest <- readId
-          pure $ c : rest
-        else pure ""
-    Nothing -> pure ""
+takeRegex :: String -> Tokenizer String
+takeRegex regex = do
+  (i, s) <- get
+  let (_, result, rest) = s =~ (regex) :: (String, String, String)
+  put (i, rest)
+  pure result
 
-readInt :: Tokenizer (Maybe String)
-readInt = do
-  res <- readChar
-  case res of
-    Just c ->
-      if isLetter c
-        then do
-          rest <- readId
-          pure $ Just (c : rest)
-        else pure (Just "")
-    Nothing -> pure Nothing
+takeToken :: Tokenizer (Maybe Token)
+takeToken =
+  (TPunct <$$> takePunct)
+    <||> (TOp <$$> takeOp)
+    <||> (TFloat <$$> takeFloat)
+    <||> (TId <$$> takeId)
+    <||> (TInt <$$> takeInt)
+
+-- | Double fmap
+(<$$>) :: (Functor f1, Functor f2) => (a -> b) -> f1 (f2 a) -> f1 (f2 b)
+a <$$> b = (fmap a) <$> b
+
+-- | Short-circuiting orElse with monads.
+(<||>) :: Monad m => m (Maybe b) -> m (Maybe b) -> m (Maybe b)
+a <||> b = do
+  a' <- a
+  case a' of
+    Nothing -> b
+    Just x -> pure $ Just x
+
+takeId :: Tokenizer (Maybe String)
+takeId = do
+  s <- takeRegex "^[a-zA-Z][a-zA-Z0-9_]*"
+  pure $ case s of
+    "" -> Nothing
+    s' -> Just s'
+
+takeInt :: Tokenizer (Maybe Integer)
+takeInt = do
+  s <- takeRegex [r|^\d+|]
+  pure $ case s of
+    "" -> Nothing
+    s' -> Just (read s' :: Integer)
+
+takeFloat :: Tokenizer (Maybe Double)
+takeFloat = do
+  s <- takeRegex "^[+-]?([0-9]*[.])?[0-9]+f?"
+  pure $ case s of
+    "" -> Nothing
+    s' -> Just (read s' :: Double)
+
+takeSpace :: Tokenizer String
+takeSpace = takeRegex [r|^\w+|]
+
+takePunct :: Tokenizer (Maybe Punct)
+takePunct = do
+  -- Peek the character on top
+  c <- peekChar
+  let result = case c of
+        Just ',' -> Just Comma
+        Just ';' -> Just Semicolon
+        Just ':' -> Just Colon
+        Just '{' -> Just LCurl
+        Just '}' -> Just RCurl
+        Just '[' -> Just LBrace
+        Just ']' -> Just RBrace
+        Just '(' -> Just LParen
+        Just ')' -> Just RParen
+        _ -> Nothing
+
+  -- If it was a punctuation character, then move forward.
+  case result of
+    Just _ -> do _ <- takeChar; pure ()
+    _ -> pure ()
+  pure result
+
+takeOp :: Tokenizer (Maybe Op)
+takeOp = do
+  s <- takeRegex [r|^[+-*/=&|^><]+|]
+  pure $ case s of
+    "+" -> Just Add
+    "-" -> Just Sub
+    "*" -> Just Mul
+    "/" -> Just Div
+    "&" -> Just BAnd
+    "|" -> Just BOr
+    "~" -> Just BNot
+    "&&" -> Just LAnd
+    "||" -> Just LOr
+    "!" -> Just LNot
+    "^" -> Just Xor
+    "++" -> Just Inc
+    "--" -> Just Dec
+    "=" -> Just Asgn
+    "==" -> Just Eq
+    "!=" -> Just Neq
+    ">" -> Just Ge
+    "<" -> Just Le
+    ">=" -> Just Geq
+    "<=" -> Just Leq
+    _ -> Nothing
